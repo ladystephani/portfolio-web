a62f6d0b976369250bd7096e2274f3a5cb51be68
lijessie <li.jessie@gmail.com>
2023-03-09 16:52:38 -0800

    Make a new header with styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Zhe Qi Li</h1>
+        <nav>
+          <ul>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Resume</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
